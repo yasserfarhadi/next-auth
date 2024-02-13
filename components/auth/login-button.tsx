5238582f0d593e08c0a4f608e57d2828fa4cdec6
yasser farhadi <yasser.farhadee@gmail.com>
2024-02-13 23:20:40 +0330

    project setup

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: 'modal' | 'redirect';
+  asChild?: boolean;
+}
+
+export const LoginButton: React.FC<LoginButtonProps> = ({
+  children,
+  mode = 'redirect',
+  asChild,
+}) => {
+  const router = useRouter();
+  function clickHandler() {
+    router.push('/auth/login');
+  }
+
+  if (mode === 'modal') {
+    return <span>TODO: Implement modal</span>;
+  }
+  return (
+    <span onClick={clickHandler} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
