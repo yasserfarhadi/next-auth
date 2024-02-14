c446fa735d124acf7b6f7947045c2644c7855640
yasser farhadi <yasser.farhadee@gmail.com>
2024-02-14 04:07:04 +0330

    prisma user and account modal

--- action/register.ts
+++ action/register.ts
@@ -4,7 +4,6 @@ import { RegisterSchema } from '@/schema';
 import * as z from 'zod';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  await new Promise((resolve) => setTimeout(() => resolve(null), 1000));
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) return { error: 'Invalid Fields' };
 
